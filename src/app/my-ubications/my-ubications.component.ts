42029e8040d085b7dbbc98ad000c3a825ce0fb84
PC <alexisperezgomez1@gmial.com>
2020-12-09 02:55:00 -0600

    ubicaciones consumidas

--- src/app/my-ubications/my-ubications.component.ts
+++ src/app/my-ubications/my-ubications.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit ,ViewChild} from '@angular/core';
 import {UserService} from 'src/app/services/user.service'
+import{UbicacionesService} from '../services/ubicaciones.service';
 
 @Component({
   selector: 'app-my-ubications',
@@ -9,13 +10,14 @@ import {UserService} from 'src/app/services/user.service'
 export class MyUbicationsComponent implements OnInit {
   id:number=0;
   token:string='';
-  ubications:[];
-  nombre="Alexis Perez Gomez"; //se encesita el nombre recuperado del componente login
+  ubications:any=[];
+  //ubicaciones: any = [];
+  nombre=""; //se encesita el nombre recuperado del componente login
   user_data:any;
   name:string='';
   lastName:string='';
   
-  constructor(private userService:UserService) { }
+  constructor(private ubicacionesService:UbicacionesService) { }
 
   ngOnInit() {
     this.user_data= JSON.parse(localStorage.getItem('usuario'));
@@ -25,15 +27,28 @@ export class MyUbicationsComponent implements OnInit {
     this.nombre=this.name+" "+this.lastName;
     console.log("el nombre es ",this.nombre);
     console.log("el token es ",this.token);
-    this.get_ubications();
+    this.get_ubications(this.token,this.user_data.id);
   }
 
-  delete_ubication(ubication:any):void{
-    
-  }
-
-  get_ubications():void{
+  get_ubications(token:string,id:number):void{
+    this.ubicacionesService.getUbicaciones(token,id)
+    .subscribe(response=>{
+      console.log("respuesta ",response)
+      this.ubications=response['ubicaciones'];
+      console.log("ubicciones ",this.ubications);
+    },error=>{
+      console.log("error ",error);
+    })
   
+  }
+  delete_ubication(ubication:any):void{
+    console.log("borrar ubicacion con el id ",ubication.idUbication);
+    this.ubicacionesService.deleteUbicaciones(this.token,ubication.idUbication)
+    .subscribe(response=>{
+      console.log("response ",response);
+    },error=>{
+      console.log("error ",error);
+    })
   }
 
 }
